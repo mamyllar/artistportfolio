16a428a1e6f13970b9adce74993e4159ca2d0866
Iiris Koivisto <koivisto.irkku2gmail.com>
2023-01-30 15:23:10 +0200

    Added ESJ

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 import './styling.css';
+
+const express = require('express');
+const app = express();
+
+const server = app.listen(8000, () => {
+    console.log(`Express running → PORT ${server.address().port}`);
+});
+
