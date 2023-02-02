5e2a3e7a4c47f762d0c8d6aeb6e397829a2da157
mamyllar <79257764+mamyllar@users.noreply.github.com>
2023-02-02 11:29:44 +0200

    First models

---
+++ models/gallery.js
@@ -0,0 +1,28 @@
+"use strict";
+
+const mongoose = require("mongoose"),
+  { Schema } = require("mongoose");
+
+var gallerySchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "Artist",
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Gallery", gallerySchema);
