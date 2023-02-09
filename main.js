a9894fdfadd81e9d44fabb285398bc54c9b763de
Iiris Koivisto <koivisto.irkku2gmail.com>
2023-02-09 14:13:18 +0200

    Edited main.js to include all dependancies

--- main.js
+++ main.js
@@ -1,9 +1,71 @@
-import './styling.css';
+"use strict";
 
-const express = require('express');
-const app = express();
+const express = require("express"),
+  layouts = require("express-ejs-layouts"),
+  app = express(),
+  router = require("./routes/index"),
+  morgan = require("morgan"),
+  mongoose = require("mongoose"),
+  methodOverride = require("method-override"),
+  passport = require("passport"),
+  cookieParser = require("cookie-parser"),
+  expressSession = require("express-session"),
+  expressValidator = require("express-validator"),
+  Artist = require("./models/artist");
 
-const server = app.listen(8000, () => {
-    console.log(`Express running → PORT ${server.address().port}`);
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb+srv://IirisAdmin:artistportfolio@artistportfolio.dar6xev.mongodb.net/test",
+  { useNewUrlParser: true, useFindAndModify: false }
+);
+mongoose.set("useCreateIndex", true);
+
+app.set("port", process.env.PORT || 3000);
+app.set("view engine", "ejs");
+
+app.use(
+  methodOverride("_method", {
+    methods: ["POST", "GET"]
+  })
+);
+
+app.use(morgan("combined"));
+app.use(layouts);
+app.use(express.static("public"));
+app.use(expressValidator());
+app.use(
+  express.urlencoded({
+    extended: false
+  })
+);
+app.use(express.json());
+
+app.use(cookieParser("secretCuisine123"));
+app.use(
+  expressSession({
+    secret: "secretCuisine123",
+    cookie: {
+      maxAge: 4000000
+    },
+    resave: false,
+    saveUninitialized: false
+  })
+);
+app.use(connectFlash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(Artist.createStrategy());
+passport.serializeArtist(Artist.serializeArtist());
+passport.deserializeArtist(Artist.deserializeArtist());
+
+app.use((req, res, next) => {
+  res.locals.loggedIn = req.isAuthenticated();
+  res.locals.currentArtist = req.Artist;
+  next();
 });
 
+app.use("/", router);
+
+const server = app.listen(app.get("port"), () => {
+    console.log(`Server running at http://localhost:${app.get("port")}`);
+  });
