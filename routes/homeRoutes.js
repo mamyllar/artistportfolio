5e684d1d5a6e652c7184f00f5497afc58e2491ab
Iiris Koivisto <koivisto.irkku2gmail.com>
2023-02-13 08:51:43 +0200

    Deleted code related to chat feature

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -3,7 +3,6 @@
 const router = require("express").Router(),
   homeController = require("../controllers/homeController");
 
-router.get("/chat", homeController.chat);
 router.get("/", homeController.index);
 
 module.exports = router;
